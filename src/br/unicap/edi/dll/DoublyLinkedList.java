6348bb59888f70515801255e780a7ec09ae7768a
GIL32610 <gil.00000032610@unicap.br>
2022-10-21 10:24:26 -0300

    insertFirst method

--- src/br/unicap/edi/dll/DoublyLinkedList.java
+++ src/br/unicap/edi/dll/DoublyLinkedList.java
@@ -6,6 +6,13 @@ public class DoublyLinkedList<T extends Comparable<T>> {
     private int quantity;
     private int capacity;
 
+    public boolean isEmpty() {
+        if (this.quantity == 0 || this.first == null || this.last == null) {
+            return true;
+        }
+        return false;
+    }
+
     public DoublyLinkedList(int capacity) {
         this.capacity = capacity;
     }
@@ -17,7 +24,18 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         return false;
     }
 
-    public void insertFirst() {
+    public void insertFirst(T content) {
+        DoublyLinkedNode<T> node = new DoublyLinkedNode(content);
+        if (this.isEmpty()) {
+            this.first = node;
+            this.last = node;
+            this.quantity++;
+        } else {
+            node.setNext(this.first);
+            this.first.setPrevious(node);
+            this.first = node;
+            this.quantity++;
+        }
 
     }
 
