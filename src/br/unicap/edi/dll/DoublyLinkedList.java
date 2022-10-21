efaa4d8778afaff458c0449cceaca2b5a9b9ed89
GIL32610 <gil.00000032610@unicap.br>
2022-10-21 10:19:24 -0300

    isFull

--- src/br/unicap/edi/dll/DoublyLinkedList.java
+++ src/br/unicap/edi/dll/DoublyLinkedList.java
@@ -4,6 +4,21 @@ public class DoublyLinkedList<T extends Comparable<T>> {
     private DoublyLinkedNode<T> first;
     private DoublyLinkedNode<T> last;
     private int quantity;
+    private int capacity;
 
+    public DoublyLinkedList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isFull() {
+        if (this.quantity == this.capacity) {
+            return true;
+        }
+        return false;
+    }
+
+    public void insertFirst() {
+
+    }
 
 }
