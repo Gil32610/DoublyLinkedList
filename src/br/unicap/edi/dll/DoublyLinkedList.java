3563c99a02a9c75f4b83d23835798689ad1f0252
GIL32610 <gil.00000032610@unicap.br>
2022-10-25 10:15:58 -0300

    insertionSort method

--- src/br/unicap/edi/dll/DoublyLinkedList.java
+++ src/br/unicap/edi/dll/DoublyLinkedList.java
@@ -92,17 +92,55 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         }
     }
 
-    public void removeLast(){
+    public void removeLast() {
         if (this.isEmpty()) {
             System.out.println("Empty list");
         } else if (this.quantity == 1) {
             this.first = null;
             this.last = null;
             this.quantity = 0;
-        }else{
+        } else {
             this.last = this.last.getPrevious();
             this.last.setNext(null);
             this.quantity--;
+        }
+    }
+
+    public void removeNode(T content) {
+
+    }
+
+    public void insertionSort(T content) {
+        DoublyLinkedNode current;
+        DoublyLinkedNode<T> node = new DoublyLinkedNode<>(content);
+        if (this.isEmpty()) {
+            this.first = node;
+            this.last = node;
+            this.quantity++;
+        } else if (content.compareTo(this.first.getContent()) < 0) {
+            this.first.setPrevious(node);
+            node.setNext(this.first);
+            this.first = node;
+            this.quantity++;
+        } else if (content.compareTo(this.last.getContent()) > 0) {
+            this.last.setNext(node);
+            node.setPrevious(this.last);
+            this.last = node;
+            this.quantity++;
+        } else {
+            current = this.first.getNext();
+
+            while (current != null) {
+                if (content.compareTo(current.getContent()) < 0) {
+                    node.setNext(current);
+                    current.getPrevious().setNext(node);
+                    current.setPrevious(node);
+                    this.quantity++;
+                }
+                else{
+                    current = current.getNext();
+                }
+            }
         }
     }
 }
